657a681df89bfb7903472d45b7ef32070262f024
gordon <gordon.lansheu@cloudysys.com>
2023-01-09 14:47:42 +0800

    init

---
+++ src/main/java/com/example/demoForDroneCI/DemoForDroneCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoForDroneCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForDroneCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForDroneCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoForDroneCI/DemoForDroneCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoForDroneCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForDroneCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
