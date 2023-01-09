2cf69cf35e0baa2176f93058ba92cce671e5d5e1
gordon <gordon.lansheu@cloudysys.com>
2023-01-09 16:11:00 +0800

    add new path

--- src/main/java/com/example/demoForDroneCI/controller/HelloController.java
+++ src/main/java/com/example/demoForDroneCI/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demoForDroneCI.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,8 @@ public class HelloController {
         return "Greetings from Spring Boot!";
     }
 
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello! " + name;
+    }
 }
